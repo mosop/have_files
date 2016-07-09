a2fda4baffd22be8a5f549118628f2eb4943932d
mosop <mosop@a.b.com>
2016-07-09 15:57:04 +0900

    0.2.2

--- src/version.cr
+++ src/version.cr
@@ -1,6 +1,3 @@
 module HaveFiles
-  VERSION = "0.2.1"
+  VERSION = "0.2.2"
 end
-
-# require "semantic_version"
-# raise "have_files can't be used under Crystal 0.18.6." if SemanticVersion.parse(Crystal::VERSION) < SemanticVersion.parse("0.18.7")
